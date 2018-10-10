ae25a99c93a28e3b0bb6a6989692d7f9526b99ca
Tonny <tonny035@hotmail.com>
2018-10-10 06:03:10 +0200

    fields and init method done in Controller

--- src/PaintApp/Controller.java
+++ src/PaintApp/Controller.java
@@ -1,31 +1,61 @@
 package PaintApp;
 
+import javafx.collections.ListChangeListener;
+import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Controller {
 
     private Stage stage;
+    Model model = new Model();
+    @FXML
+    private Canvas canvas;
+    @FXML
+    private ColorPicker colorPicker;
+    @FXML
+    private ChoiceBox brushSize;
+    @FXML
+    private ChoiceBox shapes;
+    @FXML
+    private CheckBox eraser;
 
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
     public void init() {
+        // Make our bindings here, everything is loaded.
+        model.getObservablePoints().addListener((ListChangeListener<Point2D>) c -> draw());
+        // Shapes choice box
+        model.getObservableShapes().addAll("Circle","Square");
+        shapes.setItems(model.getObservableShapes());
+        shapes.setValue("Circle"); // default shape
 
-    }
+        // default color
+        colorPicker.setValue(Color.BLACK);
 
-    public void onOpen() {
-
-    }
-
-    public void onSave() {
-    }
-
-    public void onExit() {
+        // brush size choice box
+        String j;
+        for (int i = 1; i <= 20 ; i++) {
+            j = Integer.toString(i);
+            brushSize.getItems().add(j);
+        }
+        // default size
+        brushSize.setValue("12");
     }
 
     public void canvasClicked(MouseEvent event) {
+
+    }
+
+    private void draw() {
 
     }
 
@@ -33,5 +63,17 @@ public class Controller {
     }
 
     public void redo() {
+    }
+
+    public void onOpen() {
+
+    }
+
+    public void onSave() {
+
+    }
+
+    public void onExit() {
+
     }
 }
