4bb88cf264bc7dc84d6e449bacb4dbf89b5f89f8
Tonny <tonny035@hotmail.com>
2018-10-10 05:31:06 +0200

    Main class done

--- src/PaintApp/Controller.java
+++ src/PaintApp/Controller.java
@@ -1,4 +1,17 @@
 package PaintApp;
 
+import javafx.stage.Stage;
+
 public class Controller {
+
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void init() {
+
+    }
+
 }
--- src/PaintApp/Main.java
+++ src/PaintApp/Main.java
@@ -8,14 +8,23 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private Controller controller;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+        //Set up instance instead of using static load() method
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("View.fxml"));
+        Parent root = loader.load();
+        //Now we have access to getController() through the instance... don't forget the type cast
+        controller = loader.getController();
+        controller.init();
+        controller.setStage(primaryStage);
+
+        primaryStage.setTitle("Paint App");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
